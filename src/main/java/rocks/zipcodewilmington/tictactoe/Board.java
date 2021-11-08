b717058379c78dea3653f9139d90082ad5a1c595
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-07 21:32:46 -0500

    lab

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,19 +4,104 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+
+    private Character[][] matrix;
+
+    public Board(Character[][] matrix) {         // constructor
+
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        for (int row = 0; row < matrix.length; row++) {
+            int rowCounter = 0;
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 'X') {
+                    rowCounter = rowCounter + 1;
+                    for (int i = 0; i < matrix.length; i++) {
+                        Character[] characters = matrix[i];
+                    }
+                }
+
+            }
+
+            if (rowCounter == 3) {
+                return true;
+            }
+        }
+
+        for (int col = 0; col < 3; col++) {
+            int colCounter = 0;
+            for (int row = 0; row < 3; row++) {
+                if (matrix[row][col] == 'X') {
+                    colCounter = colCounter + 1;
+                }
+            }
+            if (colCounter == 3) {
+                return true;
+            }
+        }
+
+        if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
+            return true;
+        }
+
+        if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X') {
+            return true;
+        }
+
+        return false;
     }
 
-    public Boolean isInFavorOfO() {
-        return null;
+
+    public Boolean isInFavorOfO() {    // same as above, replace o with x
+        for (int row = 0; row < matrix.length; row++) {
+            int rowCounter = 0;
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 'O') {
+                    rowCounter = rowCounter + 1;
+                    for (int i = 0; i < matrix.length; i++) {
+                        Character[] characters = matrix[i];
+                    }
+                }
+
+            }
+
+            if (rowCounter == 3) {
+                return true;
+            }
+        }
+
+        for (int col = 0; col < 3; col++) {
+            int colCounter = 0;
+            for (int row = 0; row < 3; row++) {
+                if (matrix[row][col] == 'O') {
+                    colCounter = colCounter + 1;
+                }
+            }
+            if (colCounter == 3) {
+                return true;
+            }
+        }
+
+        if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') {
+            return true;
+        }
+
+        if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O') {
+            return true;
+        }
+
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (!isInFavorOfX() && !isInFavorOfO()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public String getWinner() {
